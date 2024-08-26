01c918a6ef14aa8e6cb07a597f7cd9457a493d3e
Rahul Rawat <rahulrawat@pearlorganisation.com>
2024-08-26 15:23:29 +0530

    created banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./Layout/Home";
 
 function App() {
-  return <h1 className="bg-red-500 ">Shubham</h1>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
