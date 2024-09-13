ae6e7361f26d9dc31035e7b708c6bec423593db7
Saurabh Chamola <saurabh@pearlorganisation.com>
2024-09-13 11:15:37 +0530

    added change in Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { CiDeliveryTruck } from "react-icons/ci";
 import { CiTrophy } from "react-icons/ci";
 import { MdOutlinePayment } from "react-icons/md";
@@ -25,49 +24,49 @@ const Home = () => {
         </div>
 
         <div>
-          <div class="max-w-screen-xl mx-auto my-8 p-4 border rounded-lg">
-            <div class="flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
-              <div class="flex items-center space-x-4">
-                <div class="p-3 bg-gray-100 rounded-full">
+          <div className="max-w-screen-xl mx-auto my-8 p-4 border rounded-lg">
+            <div className="flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+              <div className="flex items-center space-x-4">
+                <div className="p-3 bg-gray-100 rounded-full">
                   <CiDeliveryTruck />
                 </div>
-                <div class="text-center md:text-left">
-                  <p class="text-sm font-semibold">FAST DELIVERY</p>
+                <div className="text-center md:text-left">
+                  <p className="text-sm font-semibold">FAST DELIVERY</p>
                 </div>
               </div>
 
-              <div class="hidden md:block w-px h-12 bg-gray-200"></div>
+              <div className="hidden md:block w-px h-12 bg-gray-200"></div>
 
-              <div class="flex items-center space-x-4">
-                <div class="p-3 bg-gray-100 rounded-full">
+              <div className="flex items-center space-x-4">
+                <div className="p-3 bg-gray-100 rounded-full">
                   <CiTrophy />
                 </div>
-                <div class="text-center md:text-left">
-                  <p class="text-sm font-semibold">TRUSTED DEALERS</p>
+                <div className="text-center md:text-left">
+                  <p className="text-sm font-semibold">TRUSTED DEALERS</p>
                 </div>
               </div>
 
-              <div class="hidden md:block w-px h-12 bg-gray-200"></div>
+              <div className="hidden md:block w-px h-12 bg-gray-200"></div>
 
-              <div class="flex items-center space-x-4">
-                <div class="p-3 bg-gray-100 rounded-full">
+              <div className="flex items-center space-x-4">
+                <div className="p-3 bg-gray-100 rounded-full">
                   <MdOutlinePayment />
                 </div>
-                <div class="text-center md:text-left">
-                  <p class="text-sm font-semibold">SECURE PAYMENT</p>
-                  <p class="text-xs text-gray-500">Your money is safe</p>
+                <div className="text-center md:text-left">
+                  <p className="text-sm font-semibold">SECURE PAYMENT</p>
+                  <p className="text-xs text-gray-500">Your money is safe</p>
                 </div>
               </div>
 
-              <div class="hidden md:block w-px h-12 bg-gray-200"></div>
+              <div className="hidden md:block w-px h-12 bg-gray-200"></div>
 
-              <div class="flex items-center space-x-4">
-                <div class="p-3 bg-gray-100 rounded-full">
+              <div className="flex items-center space-x-4">
+                <div className="p-3 bg-gray-100 rounded-full">
                   <BiSupport />
                 </div>
-                <div class="text-center md:text-left">
-                  <p class="text-sm font-semibold">SUPPORT 24/7</p>
-                  <p class="text-xs text-gray-500">Live contact/message</p>
+                <div className="text-center md:text-left">
+                  <p className="text-sm font-semibold">SUPPORT 24/7</p>
+                  <p className="text-xs text-gray-500">Live contact/message</p>
                 </div>
               </div>
             </div>
